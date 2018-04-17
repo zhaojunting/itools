ca61e7f8a2ecdfea060cc785ce82b430a30c32bc
zhaojunting1 <zhaojunting1@jd.com>
2018-04-17 15:31:45 +0800

    注释

--- java/src/main/java/java/lang/String.java
+++ java/src/main/java/java/lang/String.java
@@ -14,6 +14,10 @@ public class String {
         new String().t1();
     }
 
+//    错误: 在类 java.lang.String 中找不到 main 方法, 请将 main 方法定义为:
+//    public static void main(String[] args)
+//    否则 JavaFX 应用程序类必须扩展javafx.application.Application
+
     public void t1(){
         System.out.println("Hello world!");
     }
